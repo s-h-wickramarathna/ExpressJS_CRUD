6529e6570ee186167dcbb7ac247244e9b25a3c40
unknown <sanchithaheashan655@gmail.com>
2024-08-03 21:48:06 +0530

    Product delete From DB

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 // Model
-const Product = require('./models/productModel');
+const Product = require("./models/productModel");
 
 // routes
 app.get("/", (req, res) => {
@@ -17,38 +18,57 @@ app.get("/save", (req, res) => {
 
 // Create New Product
 app.post("/product", async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message})
-    }
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // Fetch All Products
 app.get("/product", async (req, res) => {
-    try {
-        const product = await Product.find({});
-        res.status(200).json(product);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message})
-    }
+  try {
+    const product = await Product.find({});
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // Fetch Product By ID
 app.get("/product/:id", async (req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message})
-    }
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
 });
 
+// Update Product
+app.put("/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      // Product Not Update
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with this ID ${id} ` });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
 app.listen(3000, () => {
   console.log("Server Running In Port 3000");
 });
