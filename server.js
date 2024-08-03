eb52663377daf1b23a63afd8bf1bb96b4db71e67
unknown <sanchithaheashan655@gmail.com>
2024-08-03 21:30:31 +0530

    Product Fetch BY ID created

--- server.js
+++ server.js
@@ -37,6 +37,18 @@ app.get("/product", async (req, res) => {
     }
 });
 
+// Fetch Product By ID
+app.get("/product/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+});
+
 app.listen(3000, () => {
   console.log("Server Running In Port 3000");
 });
